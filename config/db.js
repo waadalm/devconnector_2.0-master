8d2a86d28e4a8e0d79b0f7dfcc8ab391af44caf1
waadalm <almuharrami301@gmail.com>
2023-02-28 13:44:22 +0400

    first commit

---
+++ config/db.js
@@ -0,0 +1,23 @@
+const mongoose = require('mongoose');
+const config = require('config');
+const db =
+  'mongodb://admin:admin@ac-bdeljf5-shard-00-00.pryvaiy.mongodb.net:27017,ac-bdeljf5-shard-00-01.pryvaiy.mongodb.net:27017,ac-bdeljf5-shard-00-02.pryvaiy.mongodb.net:27017/?ssl=true&replicaSet=atlas-503lcm-shard-0&authSource=admin&retryWrites=true&w=majority';
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(db, {
+      useNewUrlParser: true,
+      useCreateIndex: true,
+      useFindAndModify: false,
+      useUnifiedTopology: true
+    });
+
+    console.log('MongoDB Connected...');
+  } catch (err) {
+    console.error(err.message);
+    // Exit process with failure
+    process.exit(1);
+  }
+};
+
+module.exports = connectDB;
